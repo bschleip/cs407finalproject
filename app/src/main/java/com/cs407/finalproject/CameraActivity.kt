4454bdc4e2e14dc3677fd8e19d4c6d925de0593c
bschleip <bschleip@wisc.edu>
2024-11-08 16:42:04 -0600

    braden: made add friends dialog

--- app/src/main/java/com/cs407/finalproject/CameraActivity.kt
+++ app/src/main/java/com/cs407/finalproject/CameraActivity.kt
@@ -9,8 +9,10 @@ import android.Manifest
 import android.content.Intent
 import android.net.Uri
 import android.util.Log
+import android.widget.EditText
 import android.widget.ImageButton
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCaptureException
@@ -25,6 +27,7 @@ class CameraActivity : AppCompatActivity() {
     private lateinit var captureButton: Button
     private lateinit var feedButton: Button
     private lateinit var settingsButton: ImageButton
+    private lateinit var addFriendsButton: ImageButton
     private var imageCapture: ImageCapture?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,6 +38,7 @@ class CameraActivity : AppCompatActivity() {
         captureButton = findViewById(R.id.captureButton)
         feedButton = findViewById(R.id.feedButton)
         settingsButton = findViewById(R.id.settingsButton)
+        addFriendsButton = findViewById(R.id.addFriendsButton)
 
         // Check for camera permission
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
@@ -51,6 +55,9 @@ class CameraActivity : AppCompatActivity() {
         settingsButton.setOnClickListener {
             startActivity(Intent(this, SettingsActivity::class.java))
         }
+        addFriendsButton.setOnClickListener {
+            showAddFriendsDialog()
+        }
     }
 
     private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
@@ -59,6 +66,30 @@ class CameraActivity : AppCompatActivity() {
         } else {
             Log.e("CameraActivity", "Camera permission denied")
         }
+    }
+
+    private fun showAddFriendsDialog() {
+        val dialogView = layoutInflater.inflate(R.layout.dialog_add_friends, null)
+
+        val closeButton = dialogView.findViewById<ImageButton>(R.id.closeButton)
+        val addFriendEditText = dialogView.findViewById<EditText>(R.id.addFriendsEditText)
+        val addFriendButton = dialogView.findViewById<Button>(R.id.addFriendButton)
+
+        val dialog = AlertDialog.Builder(this)
+            .setView(dialogView)
+            .create()
+
+        closeButton.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        addFriendButton.setOnClickListener {
+            val friendUsername = addFriendEditText.text.toString()
+            // TODO: handle friend searching
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
 
     private fun startCamera() {
