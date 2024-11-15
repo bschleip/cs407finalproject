2385c30d85d7911f04526bcf688b33ad7c49f9a0
choims <choims711@gmail.com>
2024-11-15 15:44:36 -0600

    changes from Min-Sung

---
+++ app/src/main/java/com/cs407/finalproject/PostHandler.kt
@@ -0,0 +1,80 @@
+package com.cs407.finalproject
+
+import android.os.Build
+import android.os.Bundle
+import android.view.HapticFeedbackConstants
+import android.view.MotionEvent
+import android.view.animation.Animation
+import android.view.animation.BounceInterpolator
+import android.view.animation.ScaleAnimation
+import android.widget.ToggleButton
+import androidx.appcompat.app.AppCompatActivity
+
+class PostHandler : AppCompatActivity() {
+    private lateinit var buttonFavorite: ToggleButton
+    private lateinit var scaleAnimation: ScaleAnimation
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.post_view)
+
+        buttonFavorite = findViewById(R.id.button_favorite)
+
+        setupAnimation()
+
+        // Setup click listener
+        buttonFavorite.setOnCheckedChangeListener { buttonView, isChecked ->
+
+            // Start animation
+            buttonView.startAnimation(scaleAnimation)
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+                buttonView.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_TAP)
+            }
+            //Update like count or perform other actions
+            updateLikeCount(isChecked)
+        }
+
+    }
+
+    private fun setupAnimation() { //scaling animation
+        scaleAnimation = ScaleAnimation(
+            0.7f, // x scale
+            1.0f,
+            0.7f, // y scale
+            1.0f,
+            Animation.RELATIVE_TO_SELF, 0.5f,
+            Animation.RELATIVE_TO_SELF, 0.5f
+        ).apply {
+            duration = 500 // Animation duration in milliseconds
+            interpolator = BounceInterpolator() // Bounce effect
+        }
+    }
+
+    private var lastTapTime: Long = 0
+
+    //TODO Implement the click handler for the like button
+//    private fun setupDoubleTapListener() {
+//        imageView.setOnTouchListener { view, event ->
+//            when (event.action) {
+//                MotionEvent.ACTION_DOWN -> {
+//                    val currentTime = System.currentTimeMillis()
+//                    val tapTime = currentTime - lastTapTime
+//                    if (tapTime < 300) {  // Double tap threshold
+//                        buttonFavorite.isChecked = true
+//                        buttonFavorite.startAnimation(scaleAnimation)
+//                        true
+//                    } else {
+//                        lastTapTime = currentTime
+//                        false
+//                    }
+//                }
+//                else -> false
+//            }
+//        }
+//    }
+
+    private fun updateLikeCount(isLiked: Boolean) {
+        // TODO implement counting logic
+    }
+}
