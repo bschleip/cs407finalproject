0bc80e9b665fc5442bc779eb38086932153fddfe
ughh1212 <anslatt4apex@gmail.com>
2024-11-18 17:32:14 -0600

    starting db

--- app/src/main/java/com/cs407/finalproject/SignupActivity.kt
+++ app/src/main/java/com/cs407/finalproject/SignupActivity.kt
@@ -1,11 +1,34 @@
 package com.cs407.finalproject
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+
 
 class SignupActivity : AppCompatActivity() {
+
+    private lateinit var usernameEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var signupButton: Button
+    private lateinit var errorMessage: TextView
+
+    private lateinit var userViewModel: UserViewModel
+
+    private lateinit var userPasswordKey: SharedPreferences
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+        usernameEditText = findViewById(R.id.usernameEditText)
+        passwordEditText = findViewById(R.id.passwordEditText)
+        signupButton = findViewById(R.id.createAccountBtn)
+        errorMessage = findViewById(R.id.errorMsgTextView)
+
+
     }
 }
