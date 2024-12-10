f716851daa80b8447e29d47e5285324bf7750db0
cjsteffen <cjsteffen@wisc.edu>
2024-12-09 23:38:27 -0600

    profile complete

--- app/src/main/java/com/cs407/finalproject/ProfileActivity.kt
+++ app/src/main/java/com/cs407/finalproject/ProfileActivity.kt
@@ -3,7 +3,9 @@ package com.cs407.finalproject
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,6 +13,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
@@ -25,6 +28,7 @@ class ProfileActivity : AppCompatActivity() {
     private lateinit var username: TextView
     private lateinit var bio: TextView
     private lateinit var recyclerView: RecyclerView
+    private lateinit var profileImage: ImageView
 
     private lateinit var databaseHelper: UserDatabaseHelper
     private var posts = listOf<Post>()
@@ -43,6 +47,7 @@ class ProfileActivity : AppCompatActivity() {
         username = findViewById(R.id.profileName)
         bio = findViewById(R.id.profileBio)
         recyclerView = findViewById(R.id.recyclerView)
+        profileImage = findViewById(R.id.profileImage)
 
         feedButton.setOnClickListener {
             startActivity(Intent(this, FeedActivity::class.java))
@@ -53,6 +58,31 @@ class ProfileActivity : AppCompatActivity() {
         profileButton.setOnClickListener {
             startActivity(Intent(this, ProfileActivity::class.java))
         }
+        profileImage.setOnClickListener {
+            //had to figure out how to get access to system images
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                val intent = Intent(MediaStore.ACTION_PICK_IMAGES)
+                imagePickerLauncher.launch(intent)
+            } else {
+                val intent = Intent(Intent.ACTION_PICK)
+                intent.type = "image/*"
+                startActivityForResult(intent, 1000)
+            }
+        }
+        bio.setOnClickListener {
+            val editText = EditText(this)
+            val dialog = AlertDialog.Builder(this)
+                .setView(editText)
+                .setPositiveButton("OK") { _, _ ->
+                    val userInput = editText.text.toString()
+                    if (userInput.isNotEmpty()) {
+                        bio.text = userInput
+                    }
+                }
+                .setNegativeButton("Cancel", null)
+                .create()
+            dialog.show()
+        }
 
         // username and posts stuff
         val userId: Int = intent.getIntExtra("USER_ID", -1).takeIf { it != -1 }
@@ -62,7 +92,9 @@ class ProfileActivity : AppCompatActivity() {
         setupProfile(userId)
         setupBioEditing()
         setupPostsGrid(userId)
+        loadProfileImage()
     }
+
 
     private fun setupProfile(userId: Int) {
         posts = databaseHelper.getPostsByUserIds(listOf(userId))
@@ -147,4 +179,44 @@ class ProfileActivity : AppCompatActivity() {
             }
         }
     }
+
+    //profile image stuff
+    private val imagePickerLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { r ->
+            if (r.resultCode == RESULT_OK) {
+                val imageUri: Uri? = r.data?.data
+                if (imageUri != null) {
+                    profileImage.setImageURI(imageUri)
+                    saveProfileImageUri(imageUri.toString())
+                }
+            }
+        }
+    private fun saveProfileImageUri(uri: String) {
+        val pref = getSharedPreferences("AppPreferences", MODE_PRIVATE)
+        with(pref.edit()) {
+            putString("PROFILE_IMAGE_URI", uri)
+            apply()
+        }
+    }
+    private fun loadProfileImage() {
+        val pref = getSharedPreferences("AppPreferences", MODE_PRIVATE)
+        val uri = pref.getString("PROFILE_IMAGE_URI", null)
+        if (!uri.isNullOrEmpty()) {
+            try {
+                val uriNew = Uri.parse(uri)
+                profileImage.setImageURI(uriNew)
+            } catch (e: Exception) {
+                profileImage.setImageResource(R.drawable.expost1)
+            }
+        } else {
+            //set it back to normal
+            profileImage.setImageResource(R.drawable.expost1)
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+        loadProfileImage()
+    }
+
+
 }
